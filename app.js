5665a11614b30643723b1c09c6cad9e9ae5733d5
Karl Grossberg <karlgro@greeny.cs.tlu.ee>
2016-03-04 16:06:34 +0200

    .

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
 (function(){
    "use strict";
    
-   var Moosipurk = function(){
+	var Moosipurk = function(){
 
-     if(Moosipurk.instance){
-       return Moosipurk.instance;
-     }
-     Moosipurk.instance = this;
-     this.routes = Moosipurk.routes;
- 
-     console.log('moosipurgi sees');
+	if(Moosipurk.instance){
+		return Moosipurk.instance;
+	}
+	Moosipurk.instance = this;
+		this.routes = Moosipurk.routes;
+		console.log('moosipurgi sees');
 
-     this.click_count = 0;
-     this.currentRoute = null;
-     console.log(this);
-     this.jars = [];
-     this.init();
-   };
+	this.click_count = 0;
+	this.currentRoute = null;
+	console.log(this);
+	this.jars = [];
+	this.init();
+	};
+   
+	window.Moosipurk = Moosipurk;
+   
+	Moosipurk.routes = {
+	 'home-view': {
+	   'render': function(){
+		 // run on load
+		 console.log('>>>>main page');
+	   }
+	},
+		'list-view': {
+	   'render': function(){
+		 console.log('>>>>list');
+	   }
+	},
+	 'manage-view': {
+	   'render': function(){
+	   }
+	}
+	};
    
    
    
-   
-   
-   //lehe laadimisel moosipurk
-   window.onload = function(){
-     var app = new Moosipurk();
-   };
+	//lehe laadimisel moosipurk
+	window.onload = function(){
+		var app = new Moosipurk();
+	};
    
 })();
