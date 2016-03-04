91548e4655c75cf1cd3420473e1bfb9adde6558d
Karl Grossberg <karlgro@greeny.cs.tlu.ee>
2016-03-04 15:54:15 +0200

    js midagi

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-app
+(function(){
+   "use strict";
+   
+   var Moosipurk = function(){
+
+     if(Moosipurk.instance){
+       return Moosipurk.instance;
+     }
+     Moosipurk.instance = this;
+     this.routes = Moosipurk.routes;
+ 
+     console.log('moosipurgi sees');
+
+     this.click_count = 0;
+     this.currentRoute = null;
+     console.log(this);
+     this.jars = [];
+     this.init();
+   };
+   
+   
+   
+   
+   
+   //lehe laadimisel moosipurk
+   window.onload = function(){
+     var app = new Moosipurk();
+   };
+   
+})();
