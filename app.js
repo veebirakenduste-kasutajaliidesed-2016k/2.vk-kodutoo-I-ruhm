08dc709e548f97b06840b1e5dfd2cc94017360cf
Raoul Kirsima <raoukir@greeny.cs.tlu.ee>
2016-03-06 11:27:06 +0200

    poolik, täna lõpetan

--- app.js
+++ app.js
@@ -1,242 +1,222 @@
 (function(){
-   "use strict";
+  "use script";
 
-   var Moosipurk = function(){
+  var Moosipurk = function(){
 
-     // SEE ON SINGLETON PATTERN
-     if(Moosipurk.instance){
-       return Moosipurk.instance;
-     }
-     //this viitab Moosipurk fn
-     Moosipurk.instance = this;
+    //see on singleton pattern
+    if(Moosipurk.instance){
+      return Moosipurk.instance;
+    }
 
-     this.routes = Moosipurk.routes;
-     // this.routes['home-view'].render()
+    //this viitab moosipurgi funktsioonile
+    Moosipurk.instance = this;
 
-     console.log('moosipurgi sees');
+    this.routes = Moosipurk.routes;
 
-     // KĆ•IK muuutujad, mida muudetakse ja on rakendusega seotud defineeritakse siin
-     this.click_count = 0;
-     this.currentRoute = null;
-     console.log(this);
+    console.log("Moosipurgi sees");
 
-     // hakkan hoidma kĆµiki purke
-     this.jars = [];
+    //kõik muutujad, mida muudetakse ja on rakendusega seotud defineeritakse siin
+    this.click_count = 0;
+    this.currentRoute = null;
+    console.log(this);
 
-     // Kui tahan Moosipurgile referenci siis kasutan THIS = MOOSIPURGI RAKENDUS ISE
-     this.init();
-   };
+    //hakkan hoidma kõiki purke
+    this.jars=[];
 
-   window.Moosipurk = Moosipurk; // Paneme muuutja kĆ¼lge
+    //kui tahan moosipurgile referenci siis kasuton THIS = MOOSIPURGI RAKENDUS ISE
+    this.init();
 
-   Moosipurk.routes = {
+  };
+
+  window.Moosipurk = Moosipurk; //paneme muutuja külge
+
+  Moosipurk.routes = {
      'home-view': {
        'render': function(){
-         // kĆ¤ivitame siis kui lehte laeme
+         // käivitame siis kui lehte laeme
          console.log('>>>>avaleht');
        }
      },
      'list-view': {
        'render': function(){
-         // kĆ¤ivitame siis kui lehte laeme
+         // käivitame siis kui lehte laeme
          console.log('>>>>loend');
 
          //simulatsioon laeb kaua
          window.setTimeout(function(){
-           document.querySelector('.loading').innerHTML = 'laetud!';
+           document.querySelector('.loading').innerHTML = 'Laetud!';
          }, 3000);
-
        }
      },
      'manage-view': {
        'render': function(){
-         // kĆ¤ivitame siis kui lehte laeme
+         // käivitame siis kui lehte laeme
        }
      }
    };
 
-   // KĆµik funktsioonid lĆ¤hevad Moosipurgi kĆ¼lge
-   Moosipurk.prototype = {
+  //kõik funktsioonid lähevad moosipurgi külge
+  Moosipurk.prototype = {
+    init: function(){
+      console.log("Rakendus läks tööle");
 
-     init: function(){
-       console.log('Rakendus lĆ¤ks tĆ¶Ć¶le');
+      //kuulan aadressirea vahetust
+      window.addEventListener('hashchange', this.routeChange.bind(this));
 
-       //kuulan aadressirea vahetust
-       window.addEventListener('hashchange', this.routeChange.bind(this));
+      //kui aadressireal ei ole hashi siis lisan juurde
+      if(!window.location.hash){
+        window.location.hash = 'home-view';
+        //routechange siin ei ole vaja sest käsitsi muutmine käivitab routechange event'i ikka
+      }else{
+        //esimesel käivitamisel vaatame urli üle ja uuendame menüüd
+        this.routeChange();
+      }
 
-       // kui aadressireal ei ole hashi siis lisan juurde
-       if(!window.location.hash){
-         window.location.hash = 'home-view';
-         // routechange siin ei ole vaja sest kĆ¤sitsi muutmine kĆ¤ivitab routechange event'i ikka
-       }else{
-         //esimesel kĆ¤ivitamisel vaatame urli Ć¼le ja uuendame menĆ¼Ć¼d
-         this.routeChange();
-       }
+      //saan kätte purgid localStorage kui on
+      if(localStorage.jars){
+        //võtan stringi ja teen tagasi objektideks
+        this.jars = JSON.parse(localStorage.jars);
+        console.log('laadisin localStorageist massiivi ' + this.jars.length);
 
-       //saan kĆ¤tte purgid localStorage kui on
-       if(localStorage.jars){
-           //vĆµtan stringi ja teen tagasi objektideks
-           this.jars = JSON.parse(localStorage.jars);
-           console.log('laadisin localStorageist massiiivi ' + this.jars.length);
+        //tekitan loendi htmli
+        this.jars.forEach(function(jar){
 
-           //tekitan loendi htmli
-           this.jars.forEach(function(jar){
+          var new_jar = new Jar(jar.title, jar.time);
 
-               var new_jar = new Jar(jar.title, jar.ingredients);
+          var li = new_jar.createHtmlElement();
+          document.querySelector('.list-of-jars').appendChild(li);
+        });
+      }
 
-               var li = new_jar.createHtmlElement();
-               document.querySelector('.list-of-jars').appendChild(li);
+      //kuulame hiireklikki nupul
+      this.bindEvents();
 
-           });
+    },
+    bindEvents: function(){
+      document.querySelector('.add-new-food').addEventListener('click',this.addNewClick.bind(this));
 
-       }
+      //kuulan trükkimist otsikastis
+      document.querySelector('#search').addEventListener('keyup', this.search.bind(this));
+    },
 
+    search: function(event){
+        //otsikasti väärtus
+        var needle = document.querySelector('#search').value;
+        console.log(needle);
 
-       // esimene loogika oleks see, et kuulame hiireklikki nupul
-       this.bindEvents();
+        var list = document.querySelectorAll('ul.list-of-jars li');
+        console.log(list);
 
-     },
+        for(var i = 0; i < list.length; i++){
 
-     bindEvents: function(){
-       document.querySelector('.add-new-jar').addEventListener('click', this.addNewClick.bind(this));
+          var li = list[i];
 
-       //kuulan trĆ¼kkimist otsikastis
-       document.querySelector('#search').addEventListener('keyup', this.search.bind(this));
+          //ühe listitemi sisu
+          var stack = li.querySelector('.content').innerHTML.toLowercase();
 
-     },
+          //kas otsisõna on sisus olemas
+          if(stack.indexOf(needle) != -1){
+            //olemas
+            li.style.display = 'list-item';
+          }else{
+            //ei ole, index on -1
+            li.style.display = 'none';
+          }
+        }
+    },
 
-     search: function(event){
-         //otsikasti vĆ¤Ć¤rtus
-         var needle = document.querySelector('#search').value.toLowerCase();
-         console.log(needle);
+    addNewClick: function(event){
+      //console.log(event);
+      var title = document.querySelector('.title').value;
+      var time = document.querySelector('.time').value;
 
-         var list = document.querySelectorAll('ul.list-of-jars li');
-         console.log(list);
+      //console.log(title + ' ' + time);
+      //1) tekitan uue Jar'i
+       var new_jar = new Jar(title, time);
 
-         for(var i = 0; i < list.length; i++){
-
-             var li = list[i];
-
-             // Ć¼he listitemi sisu tekst
-             var stack = li.querySelector('.content').innerHTML.toLowerCase();
-
-             //kas otsisĆµna on sisus olemas
-             if(stack.indexOf(needle) !== -1){
-                 //olemas
-                 li.style.display = 'list-item';
-
-             }else{
-                 //ei ole, index on -1, peidan
-                 li.style.display = 'none';
-
-             }
-
-         }
-     },
-
-     addNewClick: function(event){
-       //salvestame purgi
-       //console.log(event);
-
-       var title = document.querySelector('.title').value;
-       var ingredients = document.querySelector('.ingredients').value;
-
-       //console.log(title + ' ' + ingredients);
-       //1) tekitan uue Jar'i
-       var new_jar = new Jar(title, ingredients);
-
-       //lisan massiiivi purgi
+       //lisan massiivi purgi
        this.jars.push(new_jar);
        console.log(JSON.stringify(this.jars));
-       // JSON'i stringina salvestan localStorage'isse
+
+       //JSON'i stringina salvestan localStorage'isse
        localStorage.setItem('jars', JSON.stringify(this.jars));
 
        // 2) lisan selle htmli listi juurde
        var li = new_jar.createHtmlElement();
        document.querySelector('.list-of-jars').appendChild(li);
 
+    },
+    routeChange: function(event){
+      //kirjutan muutujasse lehe nime, võtan maha #
+      this.currentRoute = location.hash.slice(1);
+      console.log(location.currentRoute);
 
-     },
+      //kas meil on selline leht olemas?
+      if(this.routes[this.currentRoute]){
+        //muudan menüü lingi aktiivseks
+        this.updateMenu();
+        this.routes[this.currentRoute].render();
+      }else{
+        ///404 - ei olnud
 
-     routeChange: function(event){
+      }
 
-       //kirjutan muuutujasse lehe nime, vĆµtan maha #
-       this.currentRoute = location.hash.slice(1);
-       console.log(this.currentRoute);
+    },
+    updateMenu: function(){
+      //1) võtan maha aktiivse menüü lingi kui on
+      document.querySelector('.active-menu').className = document.querySelector('.active-menu').className.replace('active-menu', '');
 
-       //kas meil on selline leht olemas?
-       if(this.routes[this.currentRoute]){
+      //2) lisan uuele juurde
+      //console.log(location.hash);
+      document.querySelector('.'+this.currentRoute).className += ' active-menu';
+    }
+  }; //MOOSIPURGI LÕPP
 
-         //muudan menĆ¼Ć¼ lingi aktiivseks
-         this.updateMenu();
+  var Jar = function(new_title, new_time){
+    this.title = new_title;
+    this.time = new_time;
+    console.log('created new jar');
+  };
 
-         this.routes[this.currentRoute].render();
-
-
-       }else{
-         /// 404 - ei olnud
-       }
-
-
-     },
-
-     updateMenu: function() {
-       //http://stackoverflow.com/questions/195951/change-an-elements-class-with-javascript
-       //1) vĆµtan maha aktiivse menĆ¼Ć¼lingi kui on
-       document.querySelector('.active-menu').className = document.querySelector('.active-menu').className.replace('active-menu', '');
-
-       //2) lisan uuele juurde
-       //console.log(location.hash);
-       document.querySelector('.'+this.currentRoute).className += ' active-menu';
-
-     }
-	 
-   }; // MOOSIPURGI LĆ•PP
-
-   var Jar = function(new_title, new_ingredients){
-     this.title = new_title;
-     this.ingredients = new_ingredients;
-     console.log('created new jar');
-   };
-
-   Jar.prototype = {
-     createHtmlElement: function(){
-
-       // vĆµttes title ja ingredients ->
-       /*
-       li
+  Jar.prototype = {
+    createHtmlElement: function(){
+      //võttes title ja time ->
+      /*
+      li
         span.letter
-          M <- title esimene tĆ¤ht
+          M <- title esimene täht
         span.content
-          title | ingredients
-       */
+          title | time
+      */
 
-       var li = document.createElement('li');
+      var li = document.createElement('li');
 
-       var span = document.createElement('span');
-       span.className = 'letter';
+      var span = document.createElement('span');
+      span.className = 'letter';
 
-       var letter = document.createTextNode(this.title.charAt(0));
-       span.appendChild(letter);
+      var letter = document.createTextNode(this.title.charAt(0));
+      span.appendChild(letter);
 
-       li.appendChild(span);
+      li.appendChild(span);
 
-       var span_with_content = document.createElement('span');
-       span_with_content.className = 'content';
+      var span_with_content = document.createElement('span');
+      span_with_content.className = 'content';
 
-       var content = document.createTextNode(this.title + ' | ' + this.ingredients);
-       span_with_content.appendChild(content);
+      var content = document.createTextNode(this.title + ' | ' + this.time);
+      span_with_content.appendChild(content);
 
-       li.appendChild(span_with_content);
+      var x = document.createElement("BUTTON");
 
-       return li;
+      li.appendChild(span_with_content);
 
-     }
-   };
+      return li;
+    }
+  };
 
-   // kui leht laetud kĆ¤ivitan Moosipurgi rakenduse
-   window.onload = function(){
-     var app = new Moosipurk();
-   };
+  window.onload=function(){
+    var app = new Moosipurk();
+  };
+
+
 
 })();
