25f7cbfd199475404b3e1253f351620f7b9d66e7
Robin Ginter <robinginter@gmail.com>
2016-03-13 20:41:53 +0200

    Mostly working now...

--- todo.js
+++ todo.js
@@ -1,190 +1,154 @@
-	//alert("todo.js");
-	(function(){
-		var todoList = function(){
-			if(todoList.instance){
-				return todoList.instance;
-			}
-			todoList.instance = this;
-			this.routes = todoList.routes;
-			
-			console.log("todoList activated");
-			this.click_count = 0;
-			this.currentRoute = null;
-			this.items = [];
-			this.init();
-		};
-		window.todoList = todoList;
-		todoList.routes = {
-			'list-view':{
-				'render': function(){
+(function(){
+	"use strict";
+	//Moosipurk = List
+	//Jar = Item
+	//jars = items
+	var List = function(){
+		if(List.instance){
+			return List.instance;
+		}
+		List.instance = this;
+		this.routes = List.routes;
+		console.log('List opened');
+
+		this.click_count = 0;
+		this.currenRoute = null;
+		console.log(this);
+
+		this.item_id = 0;
+		this.items = [];
+		this.init();
+
+	};
+	window.List = List;
+	List.routes = {
+		'list-view': {
+			'render': function(){
+				console.log("List view");
 				window.setTimeout(function(){
-					document.querySelector('.loading').innerHTML = 'Done';
+					document.querySelector('.Loading...').innerHTML = 'Done!';
 				}, 3000);
-				console.log('List');
-				}
-			},
-			'manage-view': {
-				'render': function(){
-				console.log('manage');
-			   }
 			}
-		};
-		todoList.prototype = {
-			init: function(){
-				console.log("Application started");
-				window.addEventListener('hashchange', this.routeChange.bind(this));
-				if(!window.location.hash){
-					window.location.hash = 'list-view';
-					// routechange siin ei ole vaja sest käsitsi muutmine käivitab routechange event'i ikka
-				}else{
-				 //esimesel käivitamisel vaatame urli üle ja uuendame menüüd
+		},
+		'manage-view': {
+			'render': function(){
+
+			}
+		}
+	};
+	List.prototype = {
+		init: function(){
+			console.log("Init started");
+
+			window.addEventListener('hashchange', this.routeChange.bind(this));
+
+			if(!window.location.hash){
+				window.location.hash = 'list-view';
+			}else{
 				this.routeChange();
-				}
-				if(localStorage.items){
-				//Võtan stringi ja teen tagasi objektideks.
+			}
+			if(localStorage.items){
 				this.items = JSON.parse(localStorage.items);
-				console.log("Laadisin localStorage'ist massiivi " + this.items.length);
-				//Tekitan loendi htmli
+				console.log("laadisin localStorageist massiiivi ' + this.jars.length");
 				this.items.forEach(function(item){
-				var new_item = new Item(item.activity, item.time);
-				var li = new_item.createHtmlElement();
-				var buttons = document.getElementsByClassName('remove');
-				for (var i=0; i < buttons.length; i++) {
-					buttons[i].addEventListener('click', remove);
-				};
-			   document.querySelector('.list-of-items').appendChild(li);
-			 });
-		   }
-		   this.bindEvents();
-			},
-			/////////////////////////////////77777
+					var new_item = new Item(item.id, item.activity, item.time);
+
+					List.instance.item_id = item.id;
+					var li = new_item.createHtmlElement();
+					document.querySelector('.list-of-items').appendChild(li);
+				});
+				this.item_id++;
+			}
+			this.bindEvents();
+		},
 		bindEvents: function(){
-		   document.querySelector('.add-new-item').addEventListener('click', this.addNewClick.bind(this));
-		   document.querySelector('#search').addEventListener('keyup', this.search.bind(this));
-		 },
-		search: function(event){
-		   //Otsikasti väärtus
-		   var needle = document.querySelector('#search').value;
-		   console.log(needle);
-		   var list= document.querySelectorAll('.list-of-items li');
-		   //list.forEach(function(li){
-		   for(var i = 0; i < list.length; i++){
-			 var li= list[i];
-			 //Ühe listiitemi sisu tekst
-			 var stack = li.querySelector('.content').innerHTML;
-			 //Kas otsisõna on sisus olemas)
-			 if(stack.indexOf(needle) !== -1){
-			   //olemas
-			   li.style.display = 'list-item';
-			 }
-			 else{
-			   //Ei ole, index on -1
-			   li.style.display = 'none';
-			 }
-		   }
-		 },
+			document.querySelector('.add-new-item').addEventListener('click', this.addNewClick.bind(this));
+			document.querySelector('#search').addEventListener('keyup', this.search.bind(this));
+		},
+		deleteItem: function(event){
+			console.log(event.target.parentNode);
+			console.log(event.target.dataset.id);
 
-		 addNewClick: function(event){
-		   //salvestame purgi
-		   //console.log(event);
+			var clicked_li = event.target.parentNode;
+			document.querySelector('.list-of-items').removeChild(clicked_li);
+			this.items.forEach(function(item, i){
+				if(item.id == event.target.dataset.id){
+					List.instance.items.splice(i, 1);
+				}
+			});
+			localStorage.setItem('items', JSON.stringify(this.items));
+		},
+		addNewClick: function(event){
+			var activity = document.querySelector('.activity').value;
+			var time = document.querySelector('.time').value;
+			var new_item = new Item(this.item_id, activity, time);
+			this.item_id++;
+			this.items.push(new_item);
+			console.log(JSON.stringify(this.items));
+			localStorage.setItem("items", JSON.stringify(this.items));
+			var li = new_item.createHtmlElement();
+			document.querySelector('.list-of-items').appendChild(li);
+		},
+		routeChange: function(event){
+			this.currentRoute = location.hash.slice(1);
+			console.log(this.currentRoute);
+			if(this.routes[this.currentroute]){
+				this.updateMenu();
+				this.routes[this.currentRoute].render();
+			}
+			else{
+				console.log("404");
+			}
+		},
+		updateMenu: function(){
+			document.querySelector('.active-menu').className = document.querySelector('.active-menu').className.replace('active-menu', '');
+			document.querySelector('.'+this.currentRoute).className += ' active-menu';
 
-		   var activity = document.querySelector('.activity').value;
-		   var time = document.querySelector('.time').value;
-		   //console.log(title + ' ' + ingredients);
-		   //1) tekitan uue item'i
-		   var new_item = new Item(activity, time);
-		   //Lisan massiivi purgi
-		   this.items.push(new_item);
-		   console.log(JSON.stringify(this.items));
-		   //JSONi stringina salvestan local storage'isse
-		   localStorage.setItem("items", JSON.stringify(this.items));
+		}
+	};
+	//End of list
+	var Item = function(new_id, new_activity, new_time){
+		this.id = new_id;
+		this.activity = new_activity;
+		this.time = new_time;
+		console.log('New list item created');
 
-		   // 2) lisan selle htmli listi juurde
-		   var li = new_item.createHtmlElement();
-		   document.querySelector('.list-of-items').appendChild(li);
+	};
 
+	Item.prototype = {
+		createHtmlElement: function(){
+			var li = document.createElement('li');
+			var span = document.createElement('span');
+			span.className = 'letter';
+			var letter = document.createTextNode(this.activity.charAt(0));
+			span.appendChild(letter);
+			li.appendChild(span);
+			var span_with_content = document.createElement('span');
+			span_with_content.className = 'content';
+			var content = document.createTextNode(this.activity + ' | ' + this.time);
+			span_with_content.appendChild(content);
+			li.appendChild(span_with_content);
 
-		 },
-		 routeChange: function(event){
+			//delete
+			var delete_span = document.createElement('span');
+			delete_span.appendChild(document.createTextNode(' Delete'));
 
-		   //kirjutan muuutujasse lehe nime, võtan maha #
-		   this.currentRoute = location.hash.slice(1);
-		   console.log(this.currentRoute);
+			delete_span.style.color = 'red';
+			delete_span.style.cursor = 'pointer';
 
-		   //kas meil on selline leht olemas?
-		   if(this.routes[this.currentRoute]){
+			//panen kÃ¼lge id
+			delete_span.setAttribute('data-id', this.id);
 
-			 //muudan menüü lingi aktiivseks
-			 this.updateMenu();
+			delete_span.addEventListener('click', List.instance.deleteItem.bind(List.instance));
 
-			 this.routes[this.currentRoute].render();
+			li.appendChild(delete_span);
 
+				return li;
 
-		   }else{
-			 /// 404 - ei olnud
-		   }
+		}
+	};
 
-
-		 },
-		 updateMenu: function() {
-		   //http://stackoverflow.com/questions/195951/change-an-elements-class-with-javascript
-		   //1) võtan maha aktiivse menüülingi kui on
-		   document.querySelector('.active-menu').className = document.querySelector('.active-menu').className.replace('active-menu', '');
-
-		   //2) lisan uuele juurde
-		   //console.log(location.hash);
-		   document.querySelector('.'+this.currentRoute).className += ' active-menu';
-
-		 },
-		 remove: function(){
-		   var id = this.getAttribute('id');
-
-		 }
-
-	   }; //Listi lõpp
-		var Item = function(new_activity, new_time){
-		 this.activity = new_activity;
-		 this.time = new_time;
-		 console.log('New list item created');
-		};
-		/////////
-		Item.prototype = {
-		 createHtmlElement: function(){
-
-		   // võttes title ja ingredients ->
-		   /*
-		   li
-			span.letter
-			  M <- title esimene täht
-			span.content
-			  title | ingredients
-		   */
-
-		   var li = document.createElement('li');
-
-		   var span = document.createElement('span');
-		   span.className = 'letter';
-
-		   var letter = document.createTextNode(this.title.charAt(0));
-		   span.appendChild(letter);
-
-		   li.appendChild(span);
-
-		   var span_with_content = document.createElement('span');
-		   span_with_content.className = 'content';
-
-		   var content = document.createTextNode(this.activity + ' | ' + this.time);
-		   span_with_content.appendChild(content);
-
-		   li.appendChild(span_with_content);
-
-		   return li;
-	//
-		 }
-	   };
-
-	   // kui leht laetud käivitan Moosipurgi rakenduse
-	   window.onload = function(){
-		 var app = new todoList();
-	   };
-	})();
+	window.onload = function(){
+		var app = new List();
+	};
+})();
