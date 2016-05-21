e18ebdf4beefc076c9c3e570c4deb8569667d18f
Karl Grossberg <karlgro@greeny.cs.tlu.ee>
2016-05-21 21:49:41 +0300

    uus fail

--- app.js
+++ app.js
@@ -1,183 +1,217 @@
 (function(){
-    "use strict";
-    
-    var App = function(){
-        
-        if(App.instance){
-        return App.instance;
-    }
-    
-    App.instance = this;
-    
-    this.routes = App.routes;
-    console.log(this);
-    
-    this.currentRoute = null;
-    
-    this.cheeseArray = [];
-    
-    this.run();
-        
-    };
-    
-    window.App = App;
-    App.routes = {
-        'aabits':{
-            'render': function(){
-            }
-        },
-        'juustud':{
-            'render': function(){
-            }
-        },
-        'lisa': {
-            'render': function(){
-            }
+   "use strict";
+
+   var ToDoList = function(){
+
+     if(ToDoList.instance){
+       return ToDoList.instance;
+     }
+
+     ToDoList.instance = this;
+     this.routes = ToDoList.routes;
+
+
+          //muuutujad
+     this.click_count = 0;
+     this.currentRoute = null;
+     console.log(this);
+
+     this.tasks = [];
+     this.init();
+   };
+
+   window.ToDoList = ToDoList;
+
+   ToDoList.routes = {
+     'home-view': {
+       'render': function(){}
+     },
+     'list-view': {
+       'render': function(){}
+     },
+     'manage-view': {
+       'render': function(){
         }
-    };
-    
-    App.prototype = {
-        run: function(){
-          
-            window.addEventListener('hashchange', this.routeChange.bind(this));
-            if(!window.location.hash){
-                window.location.hash = 'aabits';
-            }else{
-                this.routeChange();
-            }
-                if(localStorage.cheeseArray){
-                    this.createList(JSON.parse(localStorage.cheeseArray));
-                    console.log('Laadisin localstoragest');
-                }else{
-                    console.log('midagi');
-                }
-            this.bindEvents();
-        },
-    
-    createList: function(objectCheeseArray){
-        this.cheeseArray = objectCheeseArray;
-        this.cheeseArray.forEach(function(cheeseObject2){
-            var newCheese = new cheeseObject(cheeseObject2.id, cheeseObject2.name, cheeseObject2.type, cheeseObject2.origin);
-        var li = newCheese.createHtmlElement();
-        document.querySelector('.cheese-list').appendChild(li);
-    });
-    },
-        bindEvents: function(){
-            
-            document.querySelector('.add-new-cheese').addEventListener('click', this.addNewCheese.bind(this));
-            
-            document.querySelector('#search').addEventListener('keyup', this.search.bind(this));
-            
-        },
-            
-    deleteCheese: function(event){
-            
-        console.log(event.target.parentNode);
-        console.log(event.target.dataset.id);
-        var c = confirm('Kas soovid kustutada?');
+     }
+   };
 
-        if(!c){ return; }
-        var clicked_li = event.target.parentNode;
-        document.querySelector('.cheese-list').removeChild(clicked_li);
-        this.cheeseArray.forEach(function(cheeseObject , i){
-            if(cheeseObject.id == event.target.dataset.id){
-                App.instance.cheeseArray.splice(i, 1);
-            }
-        });
-        
-        localStorage.setItem('cheeseArray', JSON.stringify(this.cheeseArray));
-    },
-    
-    changeCheese: function(event){
-        var  c = confirm('Kas soovid muuta?');
+   ToDoList.prototype = {
 
-        if(!c){ return; }
-        var li = document.createElement('li');
+     init: function(){
+       console.log('Rakendus käivitus');
 
-       var span = document.createElement('span');
-       span.className = 'letter';
+       window.addEventListener('hashchange', this.routeChange.bind(this));
+       if(!window.location.hash){
+         window.location.hash = 'home-view';
+         }else{
+       this.routeChange();
+       }
 
-        var mi = document.createElement("input");
-        mi.setAttribute('type', 'text');
-        span.appendChild(mi);
+       if(localStorage.tasks){
+           this.tasks = JSON.parse(localStorage.tasks);
+           console.log('laadisin localStorageist massiiivi ' + this.tasks.length);
 
-       li.appendChild(span);
+           this.tasks.forEach(function(task){
+
+               var new_task = new Task(task.id, task.title, task.description);
+
+               var li = new_task.createHtmlElement();
+               document.querySelector('.list-of-tasks').appendChild(li);
+
+           });
+
+       }
+      this.bindEvents();
+
+     },
+
+     bindEvents: function(){
+       document.querySelector('.add-new-task').addEventListener('click', this.addNewClick.bind(this));
+
+       document.querySelector('#search').addEventListener('keyup', this.search.bind(this));
+
+     },
+
+     deleteTask: function(event){
+
+  		//li element
+  		console.log(event.target.parentNode);
+  		//id (data-id väärtus)
+  		console.log(event.target.dataset.id);
+
+  		var c = confirm('kustuta?');
+
+  		//kui ei olnud nõus katkestame
+  		if(!c){ return; }
+
+  		//kustutame HTMList
+  		var clicked_li = event.target.parentNode;
+  		document.querySelector('.list-of-tasks').removeChild(clicked_li);
+
+  		//kustutan massiivist
+  		this.tasks.forEach(function(task, i){
+
+  			//sama id, mis vajutasime
+  			if(task.id == event.target.dataset.id){
+
+  				//mis index ja mitu. + lisaks saab asendada vajadusel
+  				//http://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_splice1
+  				ToDoList.instance.tasks.splice(i, 1);
+  			}
+
+          });
+
+  		// salvesta uuesti localStorage'isse
+         localStorage.setItem('tasks', JSON.stringify(this.tasks));
+
+	},
+     search: function(event){
+         //otsikasti väärtus
+         var needle = document.querySelector('#search').value.toLowerCase();
+         console.log(needle);
+
+         var list = document.querySelectorAll('ul.list-of-tasks li');
+         console.log(list);
+
+         for(var i = 0; i < list.length; i++){
+
+             var li = list[i];
+
+             var stack = li.querySelector('.content').innerHTML.toLowerCase();
+
+             if(stack.indexOf(needle) !== -1){
+
+                 li.style.display = 'list-item';
+
+             }else{
+                 //ei ole, index on -1, peidan
+                 li.style.display = 'none';
+
+             }
+
+         }
+     },
+
+     addNewClick: function(event){
+
+       var title = document.querySelector('.title').value;
+       var description = document.querySelector('.description').value;
+
+      var new_task = new Task(guid(), title, description);
+
+       this.tasks.push(new_task);
+       console.log(JSON.stringify(this.tasks));
+       // JSON'i stringina salvestan localStorage'isse
+       localStorage.setItem('tasks', JSON.stringify(this.tasks));
+
+       // 2) lisan selle htmli listi juurde
+       var li = new_task.createHtmlElement();
+       document.querySelector('.list-of-tasks').appendChild(li);
+// tühjendame pärast sisestamist lahtrid
+       document.querySelector('.title').value='';
+       document.querySelector('.description').value='';
+
+// liigume pärast sisestamist halduse lehele
+      window.location.hash = 'list-view';
+
+     },
+
+     routeChange: function(event){
+
+       this.currentRoute = location.hash.slice(1);
+       console.log(this.currentRoute);
+
+       if(this.routes[this.currentRoute]){
+
+         this.updateMenu();
+
+         this.routes[this.currentRoute].render();
 
 
-       return li;
-        
-    },
-        
-    
-    search: function(event){
-        var needle = document.querySelector('#search').value.toLowerCase();
-        console.log(needle);
-        var list = document.querySelectorAll('ul.cheese-list li');
-        console.log(list);
-        
-        for(var i = 0; i < list.length; i++){
-            var li = list[i];
-            var stack = li.querySelector('.content').innerHTML.toLowerCase();
-            
-            if(stack.indexOf(needle) !== -1){
-                li.style.display = 'list-item';
-            }else{
-                li.style.display = 'none';
-            }
-        }
-    },
-    
-    addNewCheese: function(event){
-        console.log(event);
-        
-        var name = document.querySelector('.name').value;
-        var type = document.querySelector('.type').value;
-        var origin = document.querySelector('.origin').value;
-        
-        var newCheese = new cheeseObject(guid(), name, type, origin);
-        console.log(JSON.stringify(this.cheeseArray));
-        this.cheeseArray.push(newCheese);
-        localStorage.setItem('cheeseArray', JSON.stringify(this.cheeseArray));
-        var li = newCheese.createHtmlElement();
-        document.querySelector('.cheese-list').appendChild(li);
-    },
-        
-    routeChange: function(event){
-        this.currentRoute = location.hash.slice(1);
-        console.log(this.currentRoute);
-        if(this.routes[this.currentRoute]){
-            this.updateMenu();
-            this.routes[this.currentRoute].render();
-        }else{
-        }
-    },
-        
+       }else{
+         /// 404 - ei olnud
+       }
+
+
+     },
+
      updateMenu: function() {
-
        document.querySelector('.active-menu').className = document.querySelector('.active-menu').className.replace('active-menu', '');
 
        document.querySelector('.'+this.currentRoute).className += ' active-menu';
 
      }
 
-};
-                                                                                
+   };
 
-    var cheeseObject = function(newId, newName, newType, newOrigin){
-        this.id = newId;
-        this.name = newName;
-        this.type = newType;
-        this.origin = newOrigin;
-    };
 
-    cheeseObject.prototype = {
-        createHtmlElement: function(){
+// ToDoListi lõpp
+
+   var Task = function(new_id, new_title, new_description){
+     this.id = new_id;
+     this.title = new_title;
+     this.description = new_description;
+     console.log('created new task');
+   };
+
+   Task.prototype = {
+     createHtmlElement: function(){
+
+       // võttes title ja description ->
+       /*
+       li
+        span.letter
+          M <- title esimene tÃ¤ht
+        span.content
+          title | description
+       */
+
        var li = document.createElement('li');
 
        var span = document.createElement('span');
        span.className = 'letter';
 
-       var letter = document.createTextNode(this.name.charAt(0));
+       var letter = document.createTextNode(this.title.charAt(0));
        span.appendChild(letter);
 
        li.appendChild(span);
@@ -185,51 +219,49 @@
        var span_with_content = document.createElement('span');
        span_with_content.className = 'content';
 
-       var content = document.createTextNode(this.name + ' | ' + this.type+ ' | ' + this.origin);
+       var content = document.createTextNode(this.title + ' | ' + this.description);
        span_with_content.appendChild(content);
 
        li.appendChild(span_with_content);
-            
+
+       //kustutamine
        var delete_span = document.createElement('span');
-	   delete_span.appendChild(document.createTextNode(' kustuta'));
-		
-	   delete_span.style.color = 'red';
-	   delete_span.style.cursor = 'pointer';
-	   
-	   delete_span.setAttribute('data-id', this.id);
-	   
-	   delete_span.addEventListener('click', App.instance.deleteCheese.bind(App.instance));
-	   
-	   li.appendChild(delete_span);
-            
-       var change_span = document.createElement('span');
-	   change_span.appendChild(document.createTextNode(' muuda'));
-            
-       change_span.style.color = 'blue';
-       change_span.style.cursor = 'pointer';
-	   change_span.addEventListener('click', App.instance.changeCheese.bind(App.instance));
-	   
-	   li.appendChild(change_span);
+       delete_span.appendChild(document.createTextNode(' kustuta'));
 
+       delete_span.style.color = 'red';
+       delete_span.style.cursor = 'pointer';
 
+       //panen külge id
+       delete_span.setAttribute('data-id', this.id);
+
+       delete_span.addEventListener('click', ToDoList.instance.deleteTask.bind(ToDoList.instance));
+
+       li.appendChild(delete_span);
        return li;
-        }
-    };
-    
-       /* HELPERID*/
-	function guid() {
-	  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-		s4() + '-' + s4() + s4() + s4();
-	}
 
-	function s4() {
-	  return Math.floor((1 + Math.random()) * 0x10000)
-		.toString(16)
-		.substring(1);
-	}
+     }
+   };
 
-    window.onload = function(){
-        var t = new App()
-        };
+
+
+
+   /* HELPERID*/
+ function guid() {
+   return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+   s4() + '-' + s4() + s4() + s4();
+ }
+
+ function s4() {
+   return Math.floor((1 + Math.random()) * 0x10000)
+   .toString(16)
+   .substring(1);
+ }
+
+
+
+   // kui leht laetud käivitub Moosipurgi rakendus
+   window.onload = function(){
+     var app = new ToDoList();
+   };
 
 })();
